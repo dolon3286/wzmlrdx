3a0e6091f805aaed8742c77245879c0f7f07d0c2
dolon3286 <itsfortrial24@gmail.com>
2026-05-27 12:12:57 +0530

    Update task_listener.py
    added TG-P emoji

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -70,6 +70,26 @@ from ..telegram_helper.message_utils import (
 )
 
 
+# --- REPLACE THESE IDs WITH REAL ONES ---
+EM_1 = '<tg-emoji emoji-id="5877219383691972108">➕</tg-emoji>'
+EM_2 = '<tg-emoji emoji-id="5195111279244619776">🍀</tg-emoji>'
+EM_3 = '<tg-emoji emoji-id="5287533898803211359">📫</tg-emoji>'
+EM_4 = '<tg-emoji emoji-id="5411590687663608498">⚡</tg-emoji>'
+EM_5 = '<tg-emoji emoji-id="5422407403884798028">🍀</tg-emoji>'
+EM_6 = '<tg-emoji emoji-id="5424867354993513047">🌷</tg-emoji>'
+EM_7 = '<tg-emoji emoji-id="5443127283898405358">⬇️</tg-emoji>'
+EM_8 = '<tg-emoji emoji-id="5445355530111437729">⬆️</tg-emoji>'
+EM_9 = '<tg-emoji emoji-id="5765005318610228026">❌</tg-emoji>'
+EM_10 = '<tg-emoji emoji-id="5285439518130857782">❤️</tg-emoji>'
+EM_11 = '<tg-emoji emoji-id="5377535110289576661">🧲</tg-emoji>'
+EM_12 = '<tg-emoji emoji-id="5357157569234216891">➕</tg-emoji>'
+EM_14 = '<tg-emoji emoji-id="5363853285809735292">🍿</tg-emoji>'
+EM_13 = '<tg-emoji emoji-id="5325547803936572038">✨</tg-emoji>'
+EM_15 = '<tg-emoji emoji-id="5424867354993513047">🌷</tg-emoji>'
+EM_16 = '<tg-emoji emoji-id="5444856076954520455">🧾</tg-emoji>'
+EM_17 = '<tg-emoji emoji-id="5224450179368767019">🌎</tg-emoji>'
+# ----------------------------------------
+
 class TaskListener(TaskConfig):
     def __init__(self):
         super().__init__()
@@ -104,19 +124,19 @@ class TaskListener(TaskConfig):
         if self.bot_pm and self.is_super_chat:
             self.pm_msg = await send_message(
                 self.user_id,
-                f"""➲ <b><u>Task Started :</u></b>
-┃
-┖ <b>Link:</b> <a href='{self.source_url}'>Click Here</a>
+                f"""{EM_16} <b><u>Task Started :</u></b>
+{EM_13}
+{EM_11} <b>Link:</b> <a href='{self.source_url}'>Click Here</a>
 """,
             )
         if Config.LINKS_LOG_ID:
             await send_message(
                 Config.LINKS_LOG_ID,
-                f"""➲  <b><u>{mode_name} Started:</u></b>
- ┃
- ┠ <b>User :</b> {self.tag} ( #ID{self.user_id} )
- ┠ <b>Message Link :</b> <a href='{self.message.link}'>Click Here</a>
- ┗ <b>Link:</b> <a href='{self.source_url}'>Click Here</a>
+                f"""{EM_13}  <b><u>{mode_name} Started:</u></b>
+ {EM_13}
+ {EM_16} <b>User :</b> {self.tag} ( #ID{self.user_id} )
+ {EM_11} <b>Message Link :</b> <a href='{self.message.link}'>Click Here</a>
+ {EM_11} <b>Link:</b> <a href='{self.source_url}'>Click Here</a>
  """,
             )
         if (
@@ -404,23 +424,23 @@ class TaskListener(TaskConfig):
         ):
             await database.rm_complete_task(self.message.link)
         msg = (
-            f"<b><i>{escape(self.name)}</i></b>\n│"
-            f"\n┟ <b>Task Size</b> → {get_readable_file_size(self.size)}"
-            f"\n┠ <b>Time Taken</b> → {get_readable_time(time() - self.message.date.timestamp())}"
-            f"\n┠ <b>In Mode</b> → {self.mode[0]}"
-            f"\n┠ <b>Out Mode</b> → {self.mode[1]}"
+            f"<b><i>{escape(self.name)}</i></b>\n{EM_13}"
+            f"\n{EM_2} <b>Task Size</b> → {get_readable_file_size(self.size)}"
+            f"\n{EM_4} <b>Time Taken</b> → {get_readable_time(time() - self.message.date.timestamp())}"
+            f"\n{EM_7} <b>In Mode</b> → {self.mode[0]}"
+            f"\n{EM_8} <b>Out Mode</b> → {self.mode[1]}"
         )
         LOGGER.info(f"Task Done: {self.name}")
         if self.is_yt:
             buttons = ButtonMaker()
             if mime_type == "Folder/Playlist":
-                msg += "\n┠ <b>Type</b> → Playlist"
-                msg += f"\n┖ <b>Total Videos</b> → {files}"
+                msg += f"\n{EM_14} <b>Type</b> → Playlist"
+                msg += f"\n{EM_1} <b>Total Videos</b> → {files}"
                 if link:
                     buttons.url_button("🔗 View Playlist", link)
                 user_message = f"{self.tag}\nYour playlist ({files} videos) has been uploaded to YouTube successfully!"
             else:
-                msg += "\n┖ <b>Type</b> → Video"
+                msg += f"\n{EM_14} <b>Type</b> → Video"
                 if link:
                     buttons.url_button("🔗 View Video", link)
                 user_message = (
@@ -439,13 +459,13 @@ class TaskListener(TaskConfig):
         elif self.is_leech:
             msg += f"\n<b>Total Files: </b>{folders}"
             if mime_type != 0:
-                msg += f"\n┠ <b>Corrupted Files</b> → {mime_type}"
-            msg += f"\n┖ <b>Task By</b> → {self.tag}\n\n"
+                msg += f"\n{EM_12} <b>Corrupted Files</b> → {mime_type}"
+            msg += f"\n{EM_16} <b>Task By</b> → {self.tag}\n\n"
 
             if self.bot_pm:
                 pmsg = msg
-                pmsg += "〶 <b><u>Action Performed :</u></b>\n"
-                pmsg += "⋗ <i>File(s) have been sent to User PM</i>\n\n"
+                pmsg += f"{EM_16} <b><u>Action Performed :</u></b>\n"
+                pmsg += f"{EM_4} <i>File(s) have been sent to User PM</i>\n\n"
                 if self.is_super_chat:
                     await send_message(self.message, pmsg)
 
@@ -453,7 +473,7 @@ class TaskListener(TaskConfig):
                 await send_message(self.message, msg)
             else:
                 log_chat = self.user_id if self.bot_pm else self.message
-                msg += "〶 <b><u>Files List :</u></b>\n"
+                msg += f"{EM_12} <b><u>Files List :</u></b>\n"
                 fmsg = ""
                 for index, (link, name) in enumerate(files.items(), start=1):
                     chat_id, msg_id = link.split("/")[-2:]
@@ -464,7 +484,7 @@ class TaskListener(TaskConfig):
                         if chat_id.isdigit():
                             chat_id = f"-100{chat_id}"
                         flink = f"https://t.me/{TgClient.BNAME}?start={encode_slink('file' + chat_id + '&&' + msg_id)}"
-                        fmsg += f"\n┖ <b>Get Media</b> → <a href='{flink}'>Store Link</a> | <a href='https://t.me/share/url?url={flink}'>Share Link</a>"
+                        fmsg += f"\n{EM_11} <b>Get Media</b> → <a href='{flink}'>Store Link</a> | <a href='https://t.me/share/url?url={flink}'>Share Link</a>"
                     fmsg += "\n"
                     if len(fmsg.encode() + msg.encode()) > 4000:
                         await send_message(log_chat, msg + fmsg)
@@ -473,10 +493,10 @@ class TaskListener(TaskConfig):
                 if fmsg != "":
                     await send_message(log_chat, msg + fmsg)
         else:
-            msg += f"\n│\n┟ <b>Type</b> → {mime_type}"
+            msg += f"\n{EM_13}\n{EM_14} <b>Type</b> → {mime_type}"
             if mime_type == "Folder":
-                msg += f"\n┠ <b>SubFolders</b> → {folders}"
-                msg += f"\n┠ <b>Files</b> → {files}"
+                msg += f"\n{EM_15} <b>SubFolders</b> → {folders}"
+                msg += f"\n{EM_12} <b>Files</b> → {files}"
 
             multi_link_msg = ""
             multi_links = []
@@ -503,7 +523,7 @@ class TaskListener(TaskConfig):
             ):
                 buttons = ButtonMaker()
                 if link and Config.SHOW_CLOUD_LINK:
-                    buttons.url_button("☁️ Cloud Link", link)
+                    buttons.url_button("{EM_4} Cloud Link", link)
                 elif multi_links:
                     for name, url in multi_links:
                         buttons.url_button(name, url)
@@ -515,7 +535,7 @@ class TaskListener(TaskConfig):
                     share_url = f"{Config.RCLONE_SERVE_URL}/{remote}/{url_path}"
                     if mime_type == "Folder":
                         share_url += "/"
-                    buttons.url_button("🔗 Rclone Link", share_url)
+                    buttons.url_button("{EM_6} Rclone Link", share_url)
                 if not rclone_path and dir_id:
                     INDEX_URL = ""
                     if self.private_link:
@@ -527,19 +547,19 @@ class TaskListener(TaskConfig):
                         share_url = f"{INDEX_URL}/{safe_name}"
                         if mime_type == "Folder" and not share_url.endswith("/"):
                                      share_url += "/"
-                        buttons.url_button("⚡ Index Link", share_url)
+                        buttons.url_button("{EM_11} Index Link", share_url)
                         if mime_type.startswith(("image", "video", "audio")):
                             share_urls = f"{share_url}?a=view"
-                            buttons.url_button("🌐 View Link", share_urls)
+                            buttons.url_button("{EM_17} View Link", share_urls)
                 button = buttons.build_menu(2)
             else:
                 if not multi_link_msg:
-                    msg += f"\n┃\n┠ Path: <code>{rclone_path}</code>"
+                    msg += f"\n{EM_13}\n{EM_6} Path: <code>{rclone_path}</code>"
                 button = None
-            msg += f"\n┃\n┖ <b>Task By</b> → {self.tag}\n\n"
+            msg += f"\n{EM_13}\n{EM_16} <b>Task By</b> → {self.tag}\n\n"
             group_msg = (
-                msg + "〶 <b><u>Action Performed :</u></b>\n"
-                "⋗ <i>Cloud link(s) have been sent to User PM</i>\n\n"
+                msg + f"{EM_13} <b><u>Action Performed :</u></b>\n"
+                f"{EM_11} <i>Cloud link(s) have been sent to User PM</i>\n\n"
             )
 
             if multi_link_msg:
@@ -587,21 +607,21 @@ class TaskListener(TaskConfig):
             count = len(task_dict)
         await self.remove_from_same_dir()
         msg = (
-            f"""〶 <b><i><u>Limit Breached:</u></i></b>
-│
-┟ <b>Task Size</b> → {get_readable_file_size(self.size)}
-┠ <b>In Mode</b> → {self.mode[0]}
-┠ <b>Out Mode</b> → {self.mode[1]}
+            f"""{EM_9} <b><i><u>Limit Breached:</u></i></b>
+{EM_13}
+{EM_2} <b>Task Size</b> → {get_readable_file_size(self.size)}
+{EM_7} <b>In Mode</b> → {self.mode[0]}
+{EM_8} <b>Out Mode</b> → {self.mode[1]}
 {error}"""
             if is_limit
-            else f"""<i><b>〶 Download Stopped!</b></i>
-│
-┟ <b>Due To</b> → {escape(str(error))}
-┠ <b>Task Size</b> → {get_readable_file_size(self.size)}
-┠ <b>Time Taken</b> → {get_readable_time(time() - self.message.date.timestamp())}
-┠ <b>In Mode</b> → {self.mode[0]}
-┠ <b>Out Mode</b> → {self.mode[1]}
-┖ <b>Task By</b> → {self.tag}"""
+            else f"""<i><b>{EM_9} Download Stopped!</b></i>
+{EM_13}
+{EM_1} <b>Due To</b> → {escape(str(error))}
+{EM_2} <b>Task Size</b> → {get_readable_file_size(self.size)}
+{EM_4} <b>Time Taken</b> → {get_readable_time(time() - self.message.date.timestamp())}
+{EM_7} <b>In Mode</b> → {self.mode[0]}
+{EM_8} <b>Out Mode</b> → {self.mode[1]}
+{EM_16} <b>Task By</b> → {self.tag}"""
         )
 
         await send_message(self.message, msg, button)
